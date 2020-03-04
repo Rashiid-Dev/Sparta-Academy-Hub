1a69a4ad1268a54a5d66fe836a961b12d4998901
Abdirashiid Jama <rashiid.py@gmail.com>
2020-03-04 00:55:43 +0000

    Added a welcome screen and a Dashboard

--- SpartaAcademyHubWPF/MainWindow.xaml.cs
+++ SpartaAcademyHubWPF/MainWindow.xaml.cs
@@ -28,17 +28,53 @@ namespace SpartaAcademyHubWPF
         Program p = new Program();
         public MainWindow()
         {
-            p.AcademiesMake();
+            //p.AcademiesMake();
             InitializeComponent();
-            //mePlayer.Play();
+            mePlayer.Source = new Uri(@"C:\Users\TECH-W150birm\source\repos\SpartaAcademyHubWPFApp\SpartaAcademyHubWPF\Spartavid.mp4");
+            mePlayer.Play();
+
+            //var count = 0;
+            //foreach (var makr in p.AcademiesMake())
+            //{
+            //    PrintInfo.Text += makr + Environment.NewLine;
+            //    count++;
+            //}
+
+
+
+        }
+
+        private void WelcomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            mePlayer.Close();
+            WelcomeButton.Visibility = Visibility.Hidden;
+            BlurryBackground.Visibility = Visibility.Visible;
+            SpartaLogo.Visibility = Visibility.Hidden;
+            AcademyLogo.Visibility = Visibility.Hidden;
+            Dashboard.Visibility = Visibility.Visible;
+        }
+
+        private void UserInfoButton_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Academy_OnClick(object sender, RoutedEventArgs e)
+        {
+            PrintInfo.Visibility = Visibility.Visible;
+            p.AcademiesMake();
             var count = 0;
             foreach (var makr in p.AcademiesMake())
             {
                 PrintInfo.Text += makr + Environment.NewLine;
                 count++;
             }
+            Dashboard.Visibility = Visibility.Hidden;
 
+        }
 
+        private void TwitterButton_OnClick(object sender, RoutedEventArgs e)
+        {
 
         }
     }
