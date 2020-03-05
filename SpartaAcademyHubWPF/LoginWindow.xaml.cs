95c83d5ea9d5d3a717b763ea520c7dee6c92a2c8
Abdirashiid Jama <rashiid.py@gmail.com>
2020-03-05 16:45:18 +0000

    Login Functionality added

--- SpartaAcademyHubWPF/LoginWindow.xaml.cs
+++ SpartaAcademyHubWPF/LoginWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Linq;
 
 namespace SpartaAcademyHubWPF
 {
@@ -17,21 +18,56 @@ namespace SpartaAcademyHubWPF
     /// </summary>
     public partial class Login : Window
     {
+        //public string selectedusername { get; set; }
+        //public string selecteduserpass { get; set; }
+        
         public Login()
         {
             InitializeComponent();
             //MainWindow mainWindow = new MainWindow();
             
+
         }
 
         private void LoginButton(object sender, RoutedEventArgs e)
         {
-            MainWindow oldwindow = new MainWindow();
-            Login LoginWindow = new Login();
-            this.Close();
-            //oldwindow.LoginButtonClicked();
-            ((MainWindow)this.Owner).LoginButtonClicked();
+           
+            var userLogin = UsernameText.Text;
+            var userPassword = PasswordText.Text;
 
+            using (var db = new AcademyHubContext())
+            {
+                
+
+                var LoginQuery =
+                //from order in db.Orders.Include(o => o.Customer)
+                //where order.Freight > 750
+                //select order;
+                from account in db.Accounts
+                where account.UserName == userLogin
+                where account.UserPass == userPassword
+                select account;
+                //if(selectedusername ==)
+                foreach (var logins in LoginQuery)
+                {
+                    if (userLogin == logins.UserName && userPassword == logins.UserPass)
+                    {
+                        MainWindow oldwindow = new MainWindow();
+                        Login LoginWindow = new Login();
+                        this.Close();
+                        //oldwindow.LoginButtonClicked();
+                        ((MainWindow)this.Owner).LoginButtonClicked();
+                    }
+                    else
+                    {
+                        RightOrWrong.Text = "Wrong Username or/and Password";
+                    }
+                }
+
+
+
+
+            }
 
         }
 
